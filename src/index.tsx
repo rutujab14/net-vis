f54cc1891b0bb9aae35fcce27b80dd27f4092a8a
rutujabg <rutujab@bu.edu>
2025-05-01 23:40:59 -0400

    Deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 //import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/net-vis">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
