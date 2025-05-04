bb20a4dbf1332efae32972941c42f5299c89e1c9
rutujab14 <rbhamare1498@gmail.com>
2025-05-04 01:48:10 -0400

    reverting refresh changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-//import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>
+  </StrictMode>
 );
