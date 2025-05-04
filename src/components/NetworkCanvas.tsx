1e3ac5e7aae0c3865fedf841125250e924a373d0
rutujab14 <rbhamare1498@gmail.com>
2025-05-04 00:47:00 -0400

    removed border of Network Canvas

--- src/components/NetworkCanvas.tsx
+++ src/components/NetworkCanvas.tsx
@@ -3,11 +3,7 @@ import { forwardRef } from "react";
 
 const NetworkCanvas = forwardRef<HTMLDivElement>((_props, ref) => {
   return (
-    <div
-      ref={ref}
-      id="network"
-      style={{ height: "600px", width: "600px", border: "1px solid #ccc" }}
-    />
+    <div ref={ref} id="network" style={{ height: "100%", width: "100%" }} />
   );
 });
 
