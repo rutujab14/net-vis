b96512bdb93dae8b587d9c445d8ef757856d9570
rutujab14 <rbhamare1498@gmail.com>
2025-05-04 01:07:41 -0400

    removed border of Network Canvas and adjusted display size

--- src/components/NetworkCanvas.tsx
+++ src/components/NetworkCanvas.tsx
@@ -3,7 +3,18 @@ import { forwardRef } from "react";
 
 const NetworkCanvas = forwardRef<HTMLDivElement>((_props, ref) => {
   return (
-    <div ref={ref} id="network" style={{ height: "100%", width: "100%" }} />
+    <div
+      ref={ref}
+      id="network"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        //border: "1px solid #ccc",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    />
   );
 });
 
