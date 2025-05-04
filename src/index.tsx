36aa4d3ad440a63af932b19f0a397f0594b37e6b
rutujab14 <rbhamare1498@gmail.com>
2025-05-04 01:21:46 -0400

    refresh at Net-vis app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>
+  </HashRouter>
 );
