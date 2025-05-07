f3ccdd8c3ee6a7b7b209eed0eca7abfbf96c40b4
rutujab14 <rbhamare1498@gmail.com>
2025-05-07 00:16:47 -0400

    refresh fix revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
 import NetworkVisualizer from "./components/NetworkVisualizer";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FaqPage from "./components/FaqPage";
 
 function App() {
   return (
-    <HashRouter basename="/net-vis">
+    <BrowserRouter basename="/net-vis">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/network-visualizer" element={<NetworkVisualizer />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
