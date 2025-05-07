27d71e6aa6d7ddb3dfd6bfd41db6c3a61dc990c8
rutujab14 <rbhamare1498@gmail.com>
2025-05-06 23:44:50 -0400

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container">
         <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a
-            href="/"
+            href="/about"
             className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
           >
             {" "}
