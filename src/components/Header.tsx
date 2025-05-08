35dc4b25e5ee15cbc531eb96cabbd08fcada5f15
rutujab14 <rbhamare1498@gmail.com>
2025-05-08 10:34:48 -0400

    App name click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,21 @@ const Header = () => {
     <>
       <div className="container">
         <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a
+          {/* <a
             href="/"
             className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
           >
-            {" "}
-            <svg className="bi me-2" width="40" height="32" aria-hidden="true">
-              <use xlinkHref="#bootstrap"></use>
-            </svg>{" "}
-            <span className="fs-4">Network Visualizer</span>{" "}
-          </a>
-
+            {" "} */}
+          <svg className="bi me-2" width="40" height="32" aria-hidden="true">
+            <use xlinkHref="#bootstrap"></use>
+          </svg>{" "}
+          <Link
+            to="/"
+            className={`nav-link ${location.pathname === "/" ? "active" : ""}`}
+          >
+            <span className="fs-4">Network Visualizer</span>
+          </Link>
+          {/* </a> */}
           <ul className="nav nav-pills">
             <li className="nav-item">
               <Link
